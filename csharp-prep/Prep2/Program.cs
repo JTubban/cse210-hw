484e506d392fe3be42f481531761fe67322fe192
jtubban <nakoa024@gmail.com>
2023-09-17 06:53:15 +0800

    My C# prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+        double grade = double.Parse(valueFromUser);
+        string letter;
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.WriteLine();
+
+        Console.WriteLine("What is your favorite number? ");
+        string valueInText = Console.ReadLine();
+        int number = int.Parse(valueInText);
+        Console.WriteLine(number);
+
     }
 }
