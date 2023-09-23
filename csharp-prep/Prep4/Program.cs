0dcb4d40d42802b490b192d0fd221b8f3797ee06
jtubban <nakoa024@gmail.com>
2023-09-23 20:57:33 +0800

    Finished Prep 04 and update sandbox

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>(); 
+        bool exit = false;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (!exit)
+        {
+            Console.Write("Enter a number: ");
+            string valueFromUser = Console.ReadLine();
+            if (valueFromUser == "0")
+            {
+                exit = true;
+            }
+            else
+            {
+                int number = int.Parse(valueFromUser);
+                numbers.Add(number);
+            }
+        }
+
+        if (exit)
+        {
+            double sumAll = 0;
+            foreach(int number in numbers)
+            {
+                sumAll += number;
+            }
+
+            double items = numbers.Count;
+            double average = sumAll / items;
+            int largest = numbers.Max();
+
+            Console.WriteLine($"The sum is: {sumAll}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {largest}");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -13,17 +14,131 @@ class Program
         // int number = int.Parse(valueInText);
         // Console.WriteLine(number);
 
-        // foreach ()
-        // {
-
-        // }
-
         // Random
         // Random randomGenerator = new Random();
         // int number = randomGenerator.Next(1, 11);
         // Console.WriteLine(number);
 
-        // 
+        // Loops
+        // string response = "yes";
+        // while (response == "yes")
+        // {
+            // Console.Write("Do you want to continue? ");
+            // response = Console.ReadLine();
+        // }
+        
+        // string response;
+        // do {
+            // Console.Write("Do you want to continue? ");
+            // response = Console.ReadLine();
+        // }while (response == "yes")
 
+        // for (int i=0; i<11; i++)
+        // {
+            // Console.WriteLine(i);
+        // }
+
+        // foreach (string color in colors)
+        // {
+            // Console.WriteLine(color);
+        // }
+
+        // LIST
+        // List<string> names = new List<string>();
+        // names.Add("James");
+        // names.Add("Juliet");
+        // names.Add("Jaxx");
+        
+        // Console.WriteLine(names.Count);
+
+        // foreach (string name in names)
+        // {
+        //     Console.WriteLine(name);
+        // }
+
+        // for (int i=0; i<names.Count; i++)
+        // {
+        //     Console.WriteLine(names[i]);
+        // }
+
+        // Getting the list size
+        // You can get the size of the list with the Count property.
+        // C#
+        // Console.WriteLine(words.Count);
+        // Python
+        // print(len(words))
+
+        // W02 ASSIGNMENT
+        // List<int> numbers = new List<int>(); 
+        // bool exit = false;
+        
+        // Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        // while (!exit)
+        // {
+        //     Console.Write("Enter a number: ");
+        //     string valueFromUser = Console.ReadLine();
+        //     if (valueFromUser == "0")
+        //     {
+        //         exit = true;
+        //     }
+        //     else
+        //     {
+        //         int number = int.Parse(valueFromUser);
+        //         numbers.Add(number);
+        //     }
+        // }
+
+        // if (exit)
+        // {
+        //     double sumAll = 0;
+        //     foreach(int number in numbers)
+        //     {
+        //         sumAll += number;
+        //     }
+
+        //     double items = numbers.Count;
+        //     double average = sumAll / items;
+        //     int largest = numbers.Max();
+
+        //     Console.WriteLine($"The sum is: {sumAll}");
+        //     Console.WriteLine($"The average is: {average}");
+        //     Console.WriteLine($"The largest number is: {largest}");
+        // }
+
+        List<int> numbers = new List<int>(); 
+        bool exit = false;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (!exit)
+        {
+            Console.Write("Enter a number: ");
+            string valueFromUser = Console.ReadLine();
+            if (valueFromUser == "0")
+            {
+                exit = true;
+            }
+            else
+            {
+                int number = int.Parse(valueFromUser);
+                numbers.Add(number);
+            }
+        }
+
+        if (exit)
+        {
+            double sumAll = 0;
+            foreach(int number in numbers)
+            {
+                sumAll += number;
+            }
+
+            double items = numbers.Count;
+            double average = sumAll / items;
+            int largest = numbers.Max();
+
+            Console.WriteLine($"The sum is: {sumAll}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {largest}");
+        }
     }
 }
