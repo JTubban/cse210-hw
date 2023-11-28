16c24b966533def59460a06c82ed42505aa18eca
jtubban <nakoa024@gmail.com>
2023-11-28 08:08:26 +0800

    Sandbox practice

---
+++ sandbox/Sandbox/BaseClass1.cs
@@ -0,0 +1,14 @@
+public class BaseClass1
+{
+    private int _number1;
+
+    public BaseClass1(int number1)
+    {
+        _number1 = number1;
+    }
+
+    public int GetNumber1()
+    {
+        return _number1;
+    }
+}
---
+++ sandbox/Sandbox/BaseClass2.cs
@@ -0,0 +1,14 @@
+public class BaseClass2
+{
+    private int _number2;
+
+    public BaseClass2(int number2)
+    {
+        _number2 = number2;
+    }
+
+    public int GetNumber1()
+    {
+        return _number2;
+    }
+}
